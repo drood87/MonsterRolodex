baa5c22c0104b2351d1376580dfd19334eb88aa5
drood87 <dbernstein1987@hotmail.com>
2019-07-12 16:28:23 +0800

    change name

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ export default class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox placeholder="search monsters" handleChange={this.handleChange} />
+        <h1>Cats Rolodex</h1>
+        <SearchBox placeholder="search cats" handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters} />
       </div>
     )
